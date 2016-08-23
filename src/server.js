3a3b8ba750edfe183a41985a8760935ef387033f
Tucker Connelly <web@tuckerconnelly.com>
2016-08-23 04:29:33 -0400

    Move from sidStore to sessionManager

--- src/server.js
+++ src/server.js
@@ -1,33 +1,21 @@
 import uid from 'uid-safe'
 
-class MemoryRedis {
+class MemorySessionManager {
   store = {};
   get(key) { return this.store[key] }
   set(key, value) { this.store[key] = value }
-  expire() {}
 }
 
 export async function createSession(store) {
   const sid = uid.sync(24)
 
-  return store.set(sid, JSON.stringify({}))
-    .then(() => store.expire(sid, global.SESSION_TTL))
-    .then(() => sid)
-}
+  await store.set(sid, JSON.stringify({}))
 
-export function bindTo(commands, ...objectsToBind) {
-  return Object.keys(commands).reduce((accumulator, current) => ({
-    ...accumulator,
-    // Bind an array of args to a function
-    // See http://stackoverflow.com/questions/21507320/using-function-prototype-bind-with-an-array-of-arguments
-    [current]: commands[current].bind.apply(
-      commands[current], [null].concat(objectsToBind)
-    ),
-  }), {})
+  return sid
 }
 
 const defaultOpts = {
-  sidStore: new MemoryRedis(),
+  sessionManager: new MemorySessionManager(),
 }
 
 export default (wss, requests, opts) => {
@@ -50,8 +38,8 @@ export default (wss, requests, opts) => {
         if (requestAction === undefined) throw new Error('Request not recognized')
 
         // Ensure there's a session
-        const session = await opts.sidStore.get(sid)
-        const guaranteedSID = session ? sid : await createSession(opts.sidStore)
+        const session = await opts.sessionManager.get(sid)
+        const guaranteedSID = session ? sid : await createSession(opts.sessionManager)
 
         const result = await requestAction(guaranteedSID, payload, extra)
         ws.send(JSON.stringify({ id, sid: guaranteedSID, result }))
