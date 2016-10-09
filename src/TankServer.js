10990fd2bbcd37f8b056f001d8ea07ea1b90b675
Tucker Connelly <web@tuckerconnelly.com>
2016-10-09 09:53:22 -0400

    Emit events

--- src/TankServer.js
+++ src/TankServer.js
@@ -1,4 +1,5 @@
 import uuid from 'uuid'
+import EventEmitter from 'eventemitter3'
 
 // Defaut implementation of a SessionManager
 // `get()` is an async function that always returns a session with an sid.
@@ -18,31 +19,46 @@ export class MemorySessionManager {
   }
 }
 
-export default class TankServer {
+export default class TankServer extends EventEmitter {
   constructor(wss, sessionManager, requests) {
+    super()
+
     this._sessionManager = sessionManager
+
 
     wss.on('connection', ws => {
       ws.on('message', async jsonMessage => {
-        const { id, sid, name, payload, extra } = JSON.parse(jsonMessage)
-
-        let requestAction
-        Object.keys(requests).forEach(availableRequest => {
-          if (name === availableRequest) requestAction = requests[availableRequest]
-        })
-
+        let request = {}
         try {
-          if (requestAction === undefined) throw new Error('Request not recognized')
+          request = JSON.parse(jsonMessage)
+          const { id, sid, name, payload, extra } = request
+
+          this.emit('request', request)
+
+          let requestAction
+          Object.keys(requests).forEach(availableRequest => {
+            if (name === availableRequest) requestAction = requests[availableRequest]
+          })
+
+          if (requestAction === undefined) {
+            this.emit('error', 'Request not recognized')
+            throw new Error('Request not recognized')
+          }
 
           // Ensure there's a session
           const session = await sessionManager.get(sid)
 
           const result = await requestAction(session.sid, payload, extra)
-          ws.send(JSON.stringify({ id, sid: session.sid, result }))
+          const response = { id, sid: session.sid, result }
+          ws.send(JSON.stringify(response))
+          this.emit('response', response, request)
         } catch (err) {
           let message = err
           if (err instanceof Error) message = err.message
-          ws.send(JSON.stringify({ id, error: message }))
+
+          const response = { id: request.id, error: message }
+          ws.send(JSON.stringify(response))
+          this.emit('response', response, request)
         }
       })
     })
